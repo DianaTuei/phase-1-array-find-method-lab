ba1793b8bd04a485f7bdabc2d84bcc2fb27a7a9c
DianaTuei <memotuei@gmail.com>
2023-12-22 05:15:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function superbowlWin(record){
+    const winningYear = record.find(record => record.result === "W");
+    return winningYear ? winningYear.year : undefined;
+
+}
+
+const record = [
+    {year: "2015", result: "W"},
+    {year: "2014", result: "N/A"},
+    {year: "2013", result: "L"},
+];
+
+const winYear = superbowlWin(record);
+console.log(winYear);
+
